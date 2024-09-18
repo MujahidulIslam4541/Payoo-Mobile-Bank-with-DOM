1c8f6e34f9b67c4787868a1ec8a1d8f0a31b1517
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2024-09-18 23:59:15 +0600

    added money section incomplete

--- js/home.js
+++ js/home.js
@@ -1,1 +1,15 @@
-console.log('added home javascript')
+
+// step:1 added by add money with event handler
+document.getElementById('add-money-btn')
+    .addEventListener('click', function(event){
+        event.preventDefault()
+        console.log('added by add money button')
+
+        // step:2 get money to be added the amount
+        const addMoney=document.getElementById('add-money-input').value;
+        console.log(addMoney);
+        // get the pin number
+        const getThePinNumber=document.getElementById('add-money-pin').value;
+        console.log(getThePinNumber);
+
+})
