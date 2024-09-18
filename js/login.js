d00b9d7cdacca1fe339cda5414873a7738c939f9
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2024-09-18 23:40:27 +0600

    Added by home page

--- js/login.js
+++ js/login.js
@@ -1,10 +1,26 @@
-// console.log('file added')
+
 
 // step:1 ---set event handler
-document.getElementById('login-button').addEventListener('click', function(){
+document.getElementById('login-button').addEventListener('click', function(event){
+
+    // step:2--added preventdefault method 
+    event.preventDefault();
     console.log('login button clicked')
 
-    // step:2--get the phone number
+    // step:3--get the phone number
     const phoneNUmber=document.getElementById('phone-number').value;
     console.log(phoneNUmber);
+
+    // step:4 added the pin number
+    const pinNumber=document.getElementById('pin-number').value;
+    console.log(pinNumber);
+
+    // step:5 this is temporary
+    if(phoneNUmber==='01' && pinNumber==='1234'){
+        console.log('You are logged in')
+        window.location.href='./home-page.html'
+    }
+    else{
+        alert('Wrong Phone Number or pin');
+    }
 })
